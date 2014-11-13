a83b070c2677b0528f01e69bab8db214fcf64d60
Alex Overbeck <alexoverbeck@gmail.com>
2014-11-13 15:25:01 -0500

    add notes and associate to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: {minimum: 8}
   validates :password_confirmation, presence: true
+
+  has_many :notes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141113193702) do
+ActiveRecord::Schema.define(version: 20141113200924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "notes", force: true do |t|
+    t.string   "title"
+    t.text     "body_html"
+    t.text     "body_text"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "notes", ["user_id"], name: "index_notes_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "name"
